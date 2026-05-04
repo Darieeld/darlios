45369747982934303d51fb4492e6ae07748423c3
Darieeld <carlos.dariel.arrieta@gmail.com>
2026-05-04 09:20:42 -0500

    URL corregida

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({ site: '[https://darlios.com](https://darlios.com)' });
+export default defineConfig({ site: 'https://darlios.com' });
