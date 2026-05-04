1bb8e1b37f0e64aa114323c1b8ffadbbd4d22d4b
Darieeld <carlos.dariel.arrieta@gmail.com>
2026-05-04 09:51:22 -0500

    Fix: base path and CNAME for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://darlios.com',
-  base: '/darlios', // IMPORTANTE: Agregamos el nombre de tu repo
+  base: '/', // Cambia '/darlios' por '/'
 });
